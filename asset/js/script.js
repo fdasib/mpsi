221a92f079fb1f744da26e02e571168919d623c8
fdasib <21faizal.farizi@poltekindonusa.ac.id>
2023-04-03 14:44:47 +0700

    03-04-2023

---
+++ asset/js/script.js
@@ -0,0 +1,9 @@
+let coba = document.querySelector("nav");
+
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+
+document.getElementById("toup").addEventListener("click", function () {
+    window.scrollTo(0, 0);
+});
